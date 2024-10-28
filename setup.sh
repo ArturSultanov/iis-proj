a49c24561b97480c42bd546bf17847e9632ad44c
Spagetik <lazo12kirill@gmail.com>
2024-10-28 14:23:52 +0100

    fix(): setup script fix

--- setup.sh
+++ setup.sh
@@ -3,10 +3,20 @@
 echo "Clearing pycache..."
 find . -type d -name "__pycache__" -exec rm -rf {} +
 
-echo "Creating a new virtual env..."
-python3 -m venv .venv
 
-source .venv/bin/activate
+# Check if a virtual environment already exists
+if [[ -d ".venv" ]]; then
+    echo "Creating a new virtual env..."
+    python3 -m venv .venv
+fi
+
+# Check if the virtual environment is activated
+
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "Activating the virtual env..."
+    source .venv/bin/activate
+fi
+
 
 if [[ -f "requirements.txt" ]]; then
     pip install --upgrade pip
