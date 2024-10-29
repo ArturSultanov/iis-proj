6bd0a582b3c692e61351fdffb786d44bb0562a16
Spagetik <lazo12kirill@gmail.com>
2024-10-29 10:49:29 +0100

    fix(): admin logic fix

--- admin.py
+++ admin.py
@@ -21,12 +21,17 @@ def get_admin(logged_user: UsersOrm = Depends(user_from_cookie)) -> UsersOrm:
 
 @admin_router.get("/dashboard")
 async def admin_page(request: Request, admin: UsersOrm = Depends(get_admin)):
-    return templates.TemplateResponse("admin_dashboard.html", {"request": request, "admin": admin}, status_code=HTTP_200_OK)
+    return templates.TemplateResponse("admin/admin_dashboard.html",
+                                      {"request": request, "admin": admin},
+                                      status_code=HTTP_200_OK)
 
 @admin_router.get("/users")
 async def users_page(request: Request, db: db_dependency, admin: UsersOrm = Depends(get_admin)):
     users = db.query(UsersOrm).all()
-    return templates.TemplateResponse("admin_users.html", {"request": request, "admin": admin, "users": users, "roles": Role.get_roles()}, status_code=HTTP_200_OK)
+    return templates.TemplateResponse("admin/admin_users.html",
+                                      {"request": request, "admin": admin, "users": users,
+                                       "roles": Role.get_roles()},
+                                      status_code=HTTP_200_OK)
 
 @admin_router.patch("/users/{user_id}/state")
 async def user_state(user_id: int, active: bool, db: db_dependency, admin: UsersOrm = Depends(get_admin)):
@@ -35,6 +40,8 @@ async def user_state(user_id: int, active: bool, db: db_dependency, admin: Users
         raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You can't change your own state")
     if not user:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
+    if user.username == "admin":
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You can't change the root admin state")
     user.disabled = active
     db.commit()
     return RedirectResponse(url="/admin/users", status_code=HTTP_202_ACCEPTED)
@@ -46,6 +53,21 @@ async def user_role(user_id: int, role: Role, db: db_dependency, admin: UsersOrm
         raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You can't change your own role")
     if not user:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
+    if user.username == "admin":
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You can't change the root admin role")
     user.role = role
+    db.commit()
+    return RedirectResponse(url="/admin/users", status_code=HTTP_202_ACCEPTED)
+
+@admin_router.delete("/users/{user_id}")
+async def delete_user(user_id: int, db: db_dependency, admin: UsersOrm = Depends(get_admin)):
+    user = db.query(UsersOrm).filter(UsersOrm.id == user_id).first()
+    if user == admin:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You can't delete yourself")
+    if not user:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
+    if user.username == "admin":
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="You can't delete the root admin")
+    db.delete(user)
     db.commit()
     return RedirectResponse(url="/admin/users", status_code=HTTP_202_ACCEPTED)
