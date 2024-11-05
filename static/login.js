b9068d6aaa63ab903893f1a79dc5908b0a636368
Spagetik <lazo12kirill@gmail.com>
2024-11-06 00:25:50 +0100

    feat(): styles

---
+++ static/login.js
@@ -0,0 +1,42 @@
+const loginForm = document.getElementById('loginForm');
+
+loginForm.addEventListener('submit', async function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+
+    const response = await fetch('/user/signin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+        window.location.href = '/user/profile';
+    } else {
+        const error = await response.json();
+        document.getElementById('error').textContent = error.detail;
+    }
+});
+
+loginForm.addEventListener('input', function () {
+    document.getElementById('error').textContent = '';
+});
+
+// get username and password from local storage
+const username = localStorage.getItem('username');
+const password = localStorage.getItem('password');
+if (username) {
+    document.getElementById('username').value = username;
+}
+if (password) {
+    document.getElementById('password').value = password;
+}
+localStorage.clear();
+
+// todo remove error on input change
