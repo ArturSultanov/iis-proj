75d0a617c7ec7e132f6f116d4c491d5690095569
lilitmovsesian <lilitmovsesian@proton.me>
2024-11-19 12:09:14 +0100

    fix: minor fixes with datetime picker

--- app/routers/vet.py
+++ app/routers/vet.py
@@ -80,14 +80,16 @@ async def treatment(request: Request, animal: animal_dependency):
 
 
 @vet_router.post("/new_treatment/{animal_id}", status_code=HTTP_201_CREATED)
-async def create_treatment(db: db_dependency, animal: animal_dependency, date: datetime = Form(...), description: str = Form(...)):
+async def create_treatment(db: db_dependency, animal: animal_dependency, date: str = Form(...), time: str = Form(...), description: str = Form(...)):
     animal_medical_history = db.query(MedicalHistoriesOrm).filter(MedicalHistoriesOrm.animal_id == animal.id).first()
     if not animal_medical_history:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Please create medical history first.")
 
+    datetime_combined = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
+
     new_treatment = TreatmentsOrm(
         medical_history_id=animal_medical_history.id,
-        date=date,
+        date=datetime_combined,
         description=description
     )
     db.add(new_treatment)
@@ -101,14 +103,16 @@ async def vaccination(request: Request, animal: animal_dependency):
 
 
 @vet_router.post("/new_vaccination/{animal_id}", status_code=HTTP_201_CREATED)
-async def create_vaccination(db: db_dependency, animal: animal_dependency, date: datetime = Form(...), description: str = Form(...)):
+async def create_vaccination(db: db_dependency, animal: animal_dependency, date: str = Form(...), time: str = Form(...), description: str = Form(...)):
     animal_medical_history = db.query(MedicalHistoriesOrm).filter(MedicalHistoriesOrm.animal_id == animal.id).first()
     if not animal_medical_history:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Please create medical history first.")
 
+    datetime_combined = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
+
     new_vaccination = VaccinationsOrm(
         medical_history_id=animal_medical_history.id,
-        date=date,
+        date=datetime_combined,
         description=description
     )
     db.add(new_vaccination)
