3006b8b247a359f94b57f0e9bc86761d35c59f90
Spagetik <lazo12kirill@gmail.com>
2024-11-21 18:22:05 +0100

    feat(): many many changes bla bla

---
+++ static/js/animal/animals_staff.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    // if no file is selected, remove the photo field
+    if (formData.get('photo').size === 0) {
+        formData.delete('photo');
+    }
+
+    // send with form data content type
+    const response = await fetch('/staff/animals/new', {
+        method: 'POST',
+        body: formData
+    });
+
+    if (response.ok && response.status === 201) {
+       window.location.reload();
+    }
+});
---
+++ static/js/signup.js
@@ -0,0 +1,40 @@
+// submit logic
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Collect form data
+    const formData = {
+        name: document.getElementById('name').value,
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value,
+        confirm_password: document.getElementById('confirm_password').value
+    };
+
+    // Send data as JSON
+    const response = await fetch('signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData) // Convert the form data to JSON
+    });
+
+    if (response.ok) {
+        window.location.href = '/user/signin';
+        localStorage.setItem('username', formData.username);
+        localStorage.setItem('password', formData.password);
+    } else {
+        const error = await response.json();
+        document.getElementById('errorMessages').textContent = error.detail || 'Registration failed';
+    }
+});
+
+// input validation
+document.getElementById('registerForm').addEventListener('input', function() {
+    const password = document.getElementById('password').value;
+    const confirm_password = document.getElementById('confirm_password').value;
+    document.getElementById('errorMessages').textContent = '';
+    if (password !== confirm_password) {
+        document.getElementById('errorMessages').textContent = 'Passwords do not match';
+    }
+});
